6cb61733ed333ab10febb0f815dd72ebff83ce5d
makoygh <124765883+makoygh@users.noreply.github.com>
2024-04-22 17:55:41 +0800

    Updates related to AI
    Display multiple facial analysis data to the view.

--- app/Http/Controllers/S3Controller.php
+++ app/Http/Controllers/S3Controller.php
@@ -179,81 +179,178 @@ class S3Controller extends Controller
 
             // extract face analysis data
             $faceDetail = json_decode($imgAnalysisData, true);
-            // dd($faceDetail[0]);
+            $faceDataCount = count($faceDetail);
 
-            $dataAgeRangeLow = $faceDetail[0]['AgeRange']['Low'];
-            $dataAgeRangeHigh = $faceDetail[0]['AgeRange']['High'];
-            $dataGender = $faceDetail[0]['Gender']['Value'];
-            $dataSmile= $this->convertToYesOrNo($faceDetail[0]['Smile']['Value']);
-            $dataEyeglasses = $this->convertToYesOrNo($faceDetail[0]['Eyeglasses']['Value']);
-            $dataFaceOccluded = $this->convertToYesOrNo($faceDetail[0]['FaceOccluded']['Value']);
-            $dataEmotions = $faceDetail[0]['Emotions'][0]['Type'];
-            $dataSunglasses = $this->convertToYesOrNo($faceDetail[0]['Sunglasses']['Value']);
-            $dataBeard = $this->convertToYesOrNo($faceDetail[0]['Beard']['Value']);
-            $dataMustache = $this->convertToYesOrNo($faceDetail[0]['Mustache']['Value']);
-            $dataEyesOpen = $this->convertToYesOrNo($faceDetail[0]['EyesOpen']['Value']);
-            $dataMouthOpen = $this->convertToYesOrNo($faceDetail[0]['MouthOpen']['Value']);
 
-            if($imgGeminiData == null){
-                $promptSmile = ' who is not smiling ';
-                if ($dataSmile=='Yes') {
-                    $promptSmile = ' who is smiling ';
+            $ctr=0;
+            $dataAgeRangeLow = '';
+            $dataAgeRangeHigh = '';
+            $dataGender = '';
+            $dataSmile = '';
+            $dataEyeglasses = '';
+            $dataFaceOccluded = '';
+            $dataEmotions = '';
+            $dataSunglasses = '';
+            $dataBeard = '';
+            $dataMustache = '';
+            $dataEyesOpen = '';
+            $dataMouthOpen = '';
+
+            $smilingCount = 0;
+            $notSmilingCount = 0;
+
+            $withEGCount = 0;
+            $withoutEGCount = 0;
+
+            $occludedFaceCount = 0;
+            $notOccludedFaceCount = 0;
+
+            $sunglassCount = 0;
+            $noSunglassCount = 0;
+
+            $beardCount = 0;
+            $noBeardCount = 0;
+
+            $mustacheCount = 0;
+            $noMustacheCount = 0;
+
+            $maleCount = 0;
+            $femaleCount = 0;
+
+            $openEyesCount = 0;
+            $closeEyesCount = 0;
+
+            $openMouthCount = 0;
+            $closeMouthCount = 0;
+
+            while($ctr < $faceDataCount){
+             
+           
+                $dataAgeRangeLow =  $faceDetail[$ctr]['AgeRange']['Low'] . ', ' . $dataAgeRangeLow;
+                $dataAgeRangeHigh = $faceDetail[$ctr]['AgeRange']['High'] . ', ' . $dataAgeRangeHigh;
+                $dataGender = $faceDetail[$ctr]['Gender']['Value'] . ', ' . $dataGender;
+
+                if($faceDetail[$ctr]['Gender']['Value'] == 'Male')
+                {
+                    $maleCount = $maleCount + 1;
+                } else {
+
+                    $femaleCount = $femaleCount + 1;
                 }
 
-                $promptEG = ' , with no eyeglass ';
-                if ($dataEyeglasses=='Yes') {
-                    $promptEG = ', wearing an eyeglass ';
+                $dataSmile= $this->convertToYesOrNo($faceDetail[$ctr]['Smile']['Value']) . ', ' . $dataSmile;
+
+                if($faceDetail[$ctr]['Smile']['Value'] == 1)
+                {
+                    $smilingCount = $smilingCount + 1;
+                } else {
+
+                    $notSmilingCount = $notSmilingCount + 1;
+                }
+                
+                $dataEyeglasses = $this->convertToYesOrNo($faceDetail[$ctr]['Eyeglasses']['Value']) . ', ' . $dataEyeglasses;
+
+                if($faceDetail[$ctr]['Eyeglasses']['Value'] == 1)
+                {
+                    $withEGCount = $withEGCount + 1;
+                } else {
+
+                    $withoutEGCount = $withoutEGCount + 1;
+                }
+                
+
+                $dataFaceOccluded = $this->convertToYesOrNo($faceDetail[$ctr]['FaceOccluded']['Value']) . ', ' . $dataFaceOccluded;
+                
+                if($faceDetail[$ctr]['FaceOccluded']['Value'] == 1)
+                {
+                    $occludedFaceCount = $occludedFaceCount + 1;
+                } else {
+
+                    $notOccludedFaceCount = $notOccludedFaceCount + 1;
+                }
+
+                $emotionsCtr =  (int) range(0, count($faceDetail[$ctr]['Emotions'])-1);
+
+                $dataEmotions = $faceDetail[$ctr]['Emotions'][$emotionsCtr]['Type'] . ', ' . $dataEmotions;
+                $dataSunglasses = $this->convertToYesOrNo($faceDetail[$ctr]['Sunglasses']['Value']) . ', ' . $dataSunglasses;
+
+                if($faceDetail[$ctr]['Sunglasses']['Value'] == 1)
+                {
+                    $sunglassCount = $sunglassCount + 1;
+                } else {
+
+                    $noSunglassCount = $noSunglassCount + 1;
+                }                
+
+                $dataBeard = $this->convertToYesOrNo($faceDetail[$ctr]['Beard']['Value']) . ', ' . $dataBeard;
+
+                if($faceDetail[$ctr]['Beard']['Value'] == 1)
+                {
+                    $beardCount = $beardCount + 1;
+                } else {
+
+                    $noBeardCount = $noBeardCount + 1;
                 }  
+
+                $dataMustache = $this->convertToYesOrNo($faceDetail[$ctr]['Mustache']['Value']) . ', ' . $dataMustache;
+
+                if($faceDetail[$ctr]['Mustache']['Value'] == 1)
+                {
+                    $mustacheCount = $mustacheCount + 1;
+                } else {
+
+                    $noMustacheCount = $noMustacheCount + 1;
+                }  
+
+                $dataEyesOpen = $this->convertToYesOrNo($faceDetail[$ctr]['EyesOpen']['Value']) . ', ' . $dataEyesOpen;
+
+                if($faceDetail[$ctr]['EyesOpen']['Value'] == 1)
+                {
+                    $openEyesCount = $openEyesCount + 1;
+                } else {
+
+                    $closeEyesCount = $closeEyesCount + 1;
+                }  
+
+                $dataMouthOpen = $this->convertToYesOrNo($faceDetail[$ctr]['MouthOpen']['Value']) . ', ' . $dataMouthOpen;
+
+                if($faceDetail[$ctr]['MouthOpen']['Value'] == 1)
+                {
+                    $openMouthCount = $openMouthCount + 1;
+                } else {
+
+                    $closeMouthCount = $closeMouthCount + 1;
+                }  
+
+                $ctr++;
+            }
+
+
+            $dataAgeRangeLow = Str::substr($dataAgeRangeLow, 1, Str::length($dataAgeRangeLow)-3);
+            $dataAgeRangeHigh = Str::substr($dataAgeRangeHigh, 0, Str::length($dataAgeRangeHigh)-2);
+            $dataGender = Str::substr($dataGender, 0, Str::length($dataGender)-2);
+            $dataSmile = Str::substr($dataSmile, 0, Str::length($dataSmile)-2);
+            $dataEyeglasses = Str::substr($dataEyeglasses, 0, Str::length($dataEyeglasses)-2);
+            $dataFaceOccluded = Str::substr($dataFaceOccluded, 0, Str::length($dataFaceOccluded)-2);
+            $dataEmotions = Str::substr($dataEmotions, 0, Str::length($dataEmotions)-2);
+            $dataSunglasses = Str::substr($dataSunglasses, 0, Str::length($dataSunglasses)-2);
+            $dataBeard = Str::substr($dataBeard, 0, Str::length($dataBeard)-2);
+            $dataMustache = Str::substr($dataMustache, 0, Str::length($dataMustache)-2);
+            $dataEyesOpen = Str::substr($dataEyesOpen, 0, Str::length($dataEyesOpen)-2);
+            $dataMouthOpen = Str::substr($dataMouthOpen, 0, Str::length($dataMouthOpen)-2);
+            
+
+            //dd($smilingCount);
+
+            if($imgGeminiData == null){
+
+
+                $promptTitle = 'Based on the following facial details or analysis:' . $imgAnalysisData . ', give us the best title for this fictional news article.';
+                $promptContent = 'Based on the following facial details or analysis:' . $imgAnalysisData . 'create a fictional news article about it in 500 words. No need for a title.';
                 
-                $promptFO = ' face is not occluded ';
-                if ($dataFaceOccluded=='Yes') {
-                    $promptFO = ', with an occluded face ';
-                }    
-
-                $promptSG = ' , with no sunglass ';
-                if ($dataSunglasses=='Yes') {
-                    $promptSG = ', wearing a sunglass ';
-                } 
-
-                $promptBeard = ' , having no beard ';
-                if ($dataSmile=='Yes') {
-                    $promptBeard = ', with a beard ';
-                } 
-
-                $promptMustache = ' , having no mustache ';
-                if ($dataMustache=='Yes') {
-                    $promptMustache = ', with a mustache ';
-                } 
-
-                $promptOE = ' , eyes are closed ';
-                if ($dataEyesOpen=='Yes') {
-                    $promptOE = ' , eyes are open ';
-                } 
-
-                $promptMO = ' , mouth is closed ';
-                if ($dataMouthOpen=='Yes') {
-                    $promptMO = ' , and mouth is open ';
-                } 
-
-                $promptAge = ', with an age between ' . $dataAgeRangeLow . ' to ' .  $dataAgeRangeHigh;
-                $promptEmotions = ', with a ' . $dataEmotions . ' emotions ';
-
-                $prompt = 'Based on the following analysis: A ' . $dataGender . ' person ' . $promptSmile;
-                $prompt = $prompt . $promptEG . $promptFO . $promptSG . $promptAge . $promptEmotions . $promptBeard . $promptMustache;
-                $prompt = $prompt . $promptOE . $promptMO . ' ';
-
-
-                $promptTitle =  $prompt . ', give me the best title for this fictional news article about that person.';;
-                $promptContent = $prompt . ', create a fictional news article about that person in 500 words. Not need for a title.';
-
-                //dd($prompt);
-                // Gemini AI
-                //$responseTitle = $this->generateNewsGemini($promptTitle);
-                //$responseContent = $this->generateNewsGemini($promptContent);
-
-                //OpenAI prompt
-                $promptOAI = $prompt . ' , create a fictional news article about that person in 500 words. Always separate title and content with a colon';
                 
+                $promptOAI = 'Based on the following analysis:' . $imgAnalysisData . ' , create a fictional news article about it in 500 words. Always separate title and content with a colon';
+
                 try{
                     if(is_null($imgGeminiData) && !is_null($imgAnalysisData)) {
                         //$responseJson = $this->generateNewsOpenAI($promptOAI);
@@ -262,8 +359,21 @@ class S3Controller extends Controller
                         // check if there is an error from OpenAI
                         if(Str::contains($responseJson, 'Error:')){
 
-                            // user Gemini AI instead
+                            // use Gemini AI instead
                             $responseTitle = $this->generateNewsGemini($promptTitle);
+
+                            if(Str::contains($responseTitle, 'Error:')){
+                                // return error message about Gemini
+                                $notification = array(
+                                    'message' => 'Error encountered when using Gemini. Please try again later.' ,
+                                    'alert-type' => 'warning'
+                                );
+
+                                return redirect()->back()->with($notification);
+
+                            }
+
+
                             $responseContent = $this->generateNewsGemini($promptContent);
 
                             //save the News Article to DB from Gemini AI
@@ -296,9 +406,33 @@ class S3Controller extends Controller
 
                 } catch (Exception $error) {
 
+                    // use Gemini AI instead
+                    $responseTitle = $this->generateNewsGemini($promptTitle);
+
+                    if(Str::contains($responseTitle, 'Error:')){
+                        // return error message about Gemini
+                        $notification = array(
+                            'message' => 'Error encountered when using Gemini. Please try again later.' ,
+                            'alert-type' => 'warning'
+                        );
+
+                        return redirect()->back()->with($notification);
+
+                    }
+                    
+                    
+                    $responseContent = $this->generateNewsGemini($promptContent);
+
+                    //save the News Article to DB from Gemini AI
+                    DB::table('s3files')
+                    ->where('s3files.id', '=', $id)
+                    ->update(['img_chatgpt_title' => $responseTitle,
+                            'img_chatgpt_content' => $responseContent,
+                            ]);   
+                            
                     // return error message about OpenAI
                     $notification = array(
-                        'message' => 'Error connecting to OpenAI.',
+                        'message' => 'Error connecting to OpenAI. Switching to Gemini AI.',
                         'alert-type' => 'error'
                     );
 
@@ -350,7 +484,27 @@ class S3Controller extends Controller
          'beard'=>$dataBeard,
          'mustache'=>$dataMustache,
          'eyesopen'=>$dataEyesOpen,
-         'mouthopen'=>$dataMouthOpen
+         'mouthopen'=>$dataMouthOpen,
+         'facecount'=>$faceDataCount,
+         'smilingcount'=>$smilingCount,
+         'notsmilingcount'=>$notSmilingCount,
+         'eyeglasscount'=>$withEGCount,
+         'noeyeglasscount'=>$withoutEGCount,
+         'occfacecount'=>$occludedFaceCount,
+         'notoccfacecount'=>$notOccludedFaceCount,
+         'sunglasscount'=>$sunglassCount,
+         'nosunglasscount'=>$noSunglassCount,
+         'beardcount'=>$beardCount,
+         'nobeardcount'=>$noBeardCount,
+         'mustachecount'=>$mustacheCount,
+         'nomustachecount'=>$noMustacheCount,
+         'malecount'=>$maleCount,
+         'femalecount'=>$femaleCount,
+         'openeyescount'=>$openEyesCount,
+         'closeeyescount'=>$closeEyesCount,
+         'openmouthcount'=>$openMouthCount,
+         'closemouthcount'=>$closeMouthCount
+
         ]);
 
     }
@@ -470,8 +624,11 @@ class S3Controller extends Controller
 
     // Generate news article using Google Gemini AI
     private function generateNewsGemini($data){
-
-        $response = Gemini::generateText($data);
+        try{
+            $response = Gemini::generateText($data);
+        } catch (Exception $error) {
+            return 'Error: ' . $error;
+        }
 
         return  $response;
 
